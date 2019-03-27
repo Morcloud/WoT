f885874453b5f95ada09fdf5c64315edef2ada84
Ricardo Tornero <richardo@outlook.com>
2019-03-26 19:22:52 -0600

    agregado de plugin dht a servidor http

--- wot-server.js
+++ wot-server.js
@@ -1,6 +1,10 @@
 let httpServer = require('./servers/http');
 let recursos = require('./recursos/modelo');
 
+let dhtPlugin = require('./plugins/sensorTempYHumDHT')
+
+dhtPlugin.start({'simulacion': true, 'frecuencia': 4000})
+
 let servidor = httpServer.listen(recursos.esp.puerto, ()=>{
     console.info(`Wot ESP está encendido y corre en el puerto ${recursos.esp.puerto}`)
 })
